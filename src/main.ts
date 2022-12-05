b2cf0e68923c3031bbcb9eca88b7a4191e535be7
tlooi <t.looi@outlook.com>
2022-12-05 23:50:43 +1300

    remove duplicate code

--- src/main.ts
+++ src/main.ts
@@ -12,10 +12,4 @@ const context = canvas.getContext('webgl');
 if (!context) {
     throw new Error('[ERROR] WebGL not supported!')
 }
-
 const R = new Renderer(context);
-R.addAttribute('a_position', 2, 0, 28);
-R.addAttribute('a_color', 3, 8, 28);
-R.addAttribute('a_texCoord', 2, 20, 28);
-R.addUniform('u_resolution', [canvas.width, canvas.height]);
-R.addUniform('u_texture', [0]);
